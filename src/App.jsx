ed68559b02d06fb49881cf16a6ae85fec84515ae
Padidham Ashwitha <ashwithapadidham17@gmail.com>
2025-07-24 16:07:06 +0530

    created using vite and configured tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="text-3xl text-green-900 font-bold underline">I'm excited to build my app named netflix gpt</h1>
+    </div>
+  )
+}
+
+export default App
